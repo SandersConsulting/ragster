b54fff258ed5b3be7ce79e15e3570fd31458ec45
Sander Nordeide <nordeidesander@gmail.com>
2023-12-25 16:37:29 +0100

    Add lists of urls to visit. write to file during runtime

--- app.py
+++ app.py
@@ -1,37 +1,80 @@
-from openai import OpenAI
 import streamlit as st
+from llama_index import VectorStoreIndex, ServiceContext
+from llama_index.llms import OpenAI
+import openai
+from llama_index import SimpleDirectoryReader
+import os
+from dotenv import find_dotenv, load_dotenv
+import debugpy
 
-st.title("ChatGPT-like clone")
+load_dotenv(find_dotenv())
+if False: # Set to False to disable debugging
+    # 5678 is the default attach port in the VS Code debug configurations
+    if not debugpy.is_client_connected():
+        debugpy.listen(5678)
+        debugpy.wait_for_client()
 
-client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
+st.set_page_config(
+    page_title="Chat with 21st.ai",
+    page_icon="💬",
+    layout="centered",
+    initial_sidebar_state="auto",
+    menu_items=None,
+)
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+st.title("Chat with 21st.ai 2️⃣1️⃣")
 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+if "messages" not in st.session_state.keys():  # Initialize the chat messages history
+    st.session_state.messages = [
+        {
+            "role": "assistant",
+            "content": "Ask me a question about 21st.ai!",
+        }
+    ]
 
+
+@st.cache_resource(show_spinner=False)
+def load_data():
+    with st.spinner(
+        text="Loading and indexing the web page – hang tight! This should take less than a minute."
+    ):
+        reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
+        docs = reader.load_data()
+        service_context = ServiceContext.from_defaults(
+            llm=OpenAI(
+                model="gpt-3.5-turbo",
+                temperature=0.5,
+                system_prompt="You are an expert on a product called 21st and your job is to answer questions about the product. Assume that all questions are related to the 21st product. Keep your answers based on facts – do not hallucinate features.",
+            )
+        )
+        index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+        return index
+
+
+index = load_data()
+
+if "chat_engine" not in st.session_state.keys():  # Initialize the chat engine
+    st.session_state.chat_engine = index.as_chat_engine(
+        chat_mode="condense_question",
+        verbose=True,
+        streaming=True,
+    )
+
+# Prompt for user input and save to chat history
+if prompt := st.chat_input("Your question"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+# Display the prior chat messages
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+        st.write(message["content"])
 
-if prompt := st.chat_input("What is up?"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
-
+# If last message is not from assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = ""
-        for response in client.chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        ):
-            full_response += response.choices[0].delta.content or ""
-            message_placeholder.markdown(full_response + "▌")
-        message_placeholder.markdown(full_response)
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+        with st.spinner("Thinking..."):
+            response = st.session_state.chat_engine.chat(prompt)
+            st.write(response.response)
+            message = {"role": "assistant", "content": response.response}
+            st.session_state.messages.append(message)  # Add response to message history
--- scrape.py
+++ scrape.py
@@ -1,6 +1,8 @@
+import math
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse
+from datetime import datetime, timedelta
 import time
 
 
@@ -18,10 +20,10 @@ def is_image_url(url):
     return url.lower().endswith((".png", ".jpg", ".jpeg", ".gif", ".bmp", ".svg"))
 
 
-def scrape_site(start_url, exclude_pattern):
+def scrape_site(urls, exclude_pattern, output_path):
     visited = set()
-    to_visit = {start_url}
-    main_domain = urlparse(start_url).netloc
+    to_visit = set(urls)
+    main_domain = urlparse(urls[0]).netloc
 
     scraped_text = ""
 
@@ -33,15 +35,24 @@ def scrape_site(start_url, exclude_pattern):
             headers = {
                 "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
             }
+            last_request =  datetime.now()
             response = requests.get(url, headers=headers)
-            
+            if response.status_code > 200:
+                print(f"Request failed for {url}: {response.status_code}")
+                continue
+
             soup = BeautifulSoup(response.content, "html.parser")
 
             # Extract and print text
-            text = soup.get_text(separator=" ", strip=True)
+            text = soup.get_text(separator="\n", strip=True) + "\n"
             scraped_text += text
             print(f"Text from {url}:")
             print(text[:500])  # Print first 500 characters of text for demonstration
+
+            # follow progress
+            with open(output_path, "wb") as file:
+                file.write(scraped_text.encode("utf-8"))
+
 
             # Find and process all links
             for link in soup.find_all("a", href=True):
@@ -52,15 +63,32 @@ def scrape_site(start_url, exclude_pattern):
                     else:
                         to_visit.add(absolute_link)
 
+            # ensure we don't make more than 1 request per 10 seconds
+            if datetime.now() - last_request < timedelta(seconds=10):
+                time.sleep(math.ceil(10 - (datetime.now()-last_request).total_seconds()) )
+
+            
+
         except requests.exceptions.RequestException as e:
             print(f"Request failed for {url}: {e}")
 
     return scraped_text
 
 
-start_url = "https://21st.bio/"
-scraped_text = scrape_site(start_url, "")
-from datetime import datetime
-timestamp = datetime.now()
-with open(f"experiments/scraped_text_{timestamp.year}-{timestamp.month}-{timestamp.day}-{timestamp.hour}-{timestamp.minute}.txt", "w") as file:
-    file.write(scraped_text)
+if __name__ == "__main__":
+    urls = [
+        "https://www.proff.no",
+        "https://www.proff.no/info/om-proff/",
+        "https://www.proff.no/info/kilder/",
+        "https://www.proff.no/info/samarbeidspartnere/",
+        "https://www.proff.no/info/hjelp-og-kontakt/",
+        "https://www.proff.no/info/alle-produkter/",
+        "https://www.proff.no/info/markedspakker/",
+        "https://www.proff.no/info/proff-api/",
+        "https://www.proff.no/info/display/",
+    ]
+    start_timestamp = datetime.now()
+    output_path = f"experiments/scraped_text_{start_timestamp.year}-{start_timestamp.month}-{start_timestamp.day}-{start_timestamp.hour}-{start_timestamp.minute}.txt"
+    scraped_text = scrape_site(urls, " ", output_path=output_path)
+    
+    
